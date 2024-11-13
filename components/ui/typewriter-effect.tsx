4ce6446ae42302946af40195c3227b5623974706
numi8462 <yhk8462@naver.com>
2024-11-13 22:29:38 +0900

    update

--- components/ui/typewriter-effect.tsx
+++ components/ui/typewriter-effect.tsx
@@ -37,7 +37,7 @@ export const TypewriterEffect = ({
         },
         {
           duration: 0.5,
-          delay: stagger(0.1),
+          delay: stagger(0.1, { startDelay: 2 }),
           ease: "easeInOut",
         }
       );
